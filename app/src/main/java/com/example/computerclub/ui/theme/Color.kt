462a8948ceaee384dff7213f91e63acefb96e1ee
Roman <romankashta4ik@gmail.com>
2026-05-20 11:40:00 +0300

    Настроена тема приложения

--- app/src/main/java/com/example/computerclub/ui/theme/Color.kt
+++ app/src/main/java/com/example/computerclub/ui/theme/Color.kt
@@ -2,10 +2,31 @@ package com.example.computerclub.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val GamerBlack = Color(0xFF0B0B0F)
+val GamerDark = Color(0xFF141418)
+val GamerCard = Color(0xFF1C1C22)
+
+val GamerRed = Color(0xFFFF3344)
+val GamerDarkRed = Color(0xFFB00020)
+val GamerNeonRed = Color(0xFFFF1744)
+
+val GamerText = Color(0xFFF5F5F5)
+val GamerTextSecondary = Color(0xFFB7B7B7)
+val GamerBorder = Color(0xFF333333)
+val ClubSurfaceLight = Color(0xFFF6F8FA)
+
+val LightPrimary = Color(0xFFD7192A)
+val LightSecondary = Color(0xFF5A0B14)
+
+val HomeBg = Color(0xFF090A0F)
+val HomeCardBg = Color(0xFF1D1E24)
+val HomeFieldBg = Color(0xFF15161B)
+val HomeRed = Color(0xFFFF3347)
+val HomeGreen = Color(0xFF25D875)
+val HomeMuted = Color(0xFF8F8F96)
+val HomeStroke = Color(0xFF34353C)
+val HomeAuthTop = Color(0xFF101116)
+val HomeAuthBorder = Color(0xFF32333A)
+val HomeInputBorder = Color(0xFF3A3B42)
+val HomeSegmentBg = Color(0xFF202128)
